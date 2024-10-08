00c074cd3001521b9c59c0324177bcb399b8aee0
raffico <raffael.vieira@escola.pr.gov.br>
2024-10-08 08:18:06 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Título',
+    'Pergunta',
+    'Resposta'
+)
+criaCartao(
+    'Batata',
+    'Você gosta de batata?',
+    'Batatas são legais'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
